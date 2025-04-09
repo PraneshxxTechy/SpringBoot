1fb54d2fcbb40be10c2a9801f549a75a09e54979
PraneshxxTechy <120735988+PraneshxxTechy@users.noreply.github.com>
2025-04-09 15:41:42 +0530

    Message util class is developed

--- demo/src/main/java/com/project/bookApp/util/MessageUtil.java
+++ demo/src/main/java/com/project/bookApp/util/MessageUtil.java
@@ -1,4 +1,13 @@
 package com.project.bookApp.util;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MessageUtil {
+    @Value("${app.greeting}")
+    private String greeting;
+    public String getGreeting(){
+        return greeting;
+    }
 }
