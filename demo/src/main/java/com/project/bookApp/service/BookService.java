3623396a93293d9947a56c37b378f57e367eaca4
PraneshxxTechy <120735988+PraneshxxTechy@users.noreply.github.com>
2025-04-09 15:06:55 +0530

    Service layer of the project has been implemented.

--- demo/src/main/java/com/project/bookApp/service/BookService.java
+++ demo/src/main/java/com/project/bookApp/service/BookService.java
@@ -1,4 +1,20 @@
 package com.project.bookApp.service;
+import java.util.*;
+import com.project.bookApp.model.Book;
+import com.project.bookApp.BookApplication;
+import com.project.bookApp.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookService {
+    private final BookRepository bookRepository;
+    @Autowired
+    public BookService(@Qualifier("dbRepo")BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
+    public List<Book> getBooks(){
+        return bookRepository.findBooks();
+    }
 }
